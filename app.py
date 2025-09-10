6702669ed7abd7b04c2ac6450c46f41a91e573d6
jamilEmon <jamil.mu.cse@gmail.com>
2025-09-10 15:12:05 +0600

    update

--- app.py
+++ app.py
@@ -91,5 +91,5 @@ def predict(
 
 # Run app on Render
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render auto sets PORT
+    port = int(os.environ.get("PORT", 4000))  # Render auto sets PORT
     uvicorn.run(app, host="0.0.0.0", port=port)
